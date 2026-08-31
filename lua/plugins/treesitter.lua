8b5e2ceee8d838486f4bdeab90714a21e636481d
Chandler <chandlerjweeks0@gmail.com>
2026-08-31 14:30:01 -0400

    fix rust treesitter bootstrap

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,19 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
+      local treesitter = require("nvim-treesitter")
+
+      if not vim.list_contains(treesitter.get_installed("parsers"), "rust") then
+        treesitter.install({ "rust" }):wait(300000)
+      end
+
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "rust",
         callback = function()
-          vim.treesitter.start()
+          local ok, err = pcall(vim.treesitter.start)
+          if not ok then
+            vim.notify("Unable to start Rust Treesitter: " .. err, vim.log.levels.WARN)
+          end
         end,
       })
     end,
